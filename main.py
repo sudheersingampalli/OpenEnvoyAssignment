df5d5b3512b55d51961b1b44c3173cb86d250ec0
Sudheer Singampalli <singampallisudheer@gmail.com>
2024-01-31 17:04:50 +0530

    Update main.py
    adds code to call the respective class from filename extension by removing if conditions

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from pythonLineCounter import PythonLineCounter
 from javaLineCounter import JavaLineCounter
+from mapper import fileTypeDict
+import os
 
 if __name__ == "__main__":
-    # Example usage for Python source file
-    print('Python file details')
-    python_counter = PythonLineCounter("pythontestfile.py")
-    python_counter.count_lines()
 
-    #  Example usage for Java source file
-    print('Java file details')
-    java_counter = JavaLineCounter("javatestfile.java")
-    java_counter.count_lines()
+    fileName = input("enter file name")
+    _ , fileType = os.path.splitext(fileName)
+
+    classType = fileTypeDict[fileType]
+    classType(fileName).count_lines()
+    # if fileType == 'py':
+    #     print('Python file details')
+    #     python_counter = PythonLineCounter("pythontestfile.py")
+    #     python_counter.count_lines()
+
+
+    # #  Example usage for Java source file
+    # print('Java file details')
+    # java_counter = JavaLineCounter("javatestfile.java")
+    # java_counter.count_lines()
