108ac4959cc62e0fb01809c096dd090ca9baf4f4
Sudheer Singampalli <singampallisudheer@gmail.com>
2024-01-31 17:06:08 +0530

    Update pythonLineCounter.py
    adds code to handle multiline comments

--- pythonLineCounter.py
+++ pythonLineCounter.py
@@ -2,9 +2,20 @@ from linesOfCodeCounter import LineCounter
 
 class PythonLineCounter(LineCounter):
     def process_line(self, line):
-        if not line:
-            self.blank_lines += 1
-        elif line.startswith('#'):
-            self.comment_lines += 1
-        else:
-            self.code_lines += 1
+        if self.multiline_start == False:
+            if not line:
+                self.blank_lines += 1
+            elif line.startswith('#'):
+                self.comment_lines += 1
+            elif line.startswith("'''"):
+                self.multiline_start =True
+                self.multi_comment_lines += 1
+                return
+            else:
+                self.code_lines += 1
+        
+        if self.multiline_start == True:
+            if line.startswith("'''"):
+                self.multiline_start = False
+            self.multi_comment_lines += 1
+
